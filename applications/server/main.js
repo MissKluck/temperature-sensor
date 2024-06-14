d0f87b887b790909ec43c9f491a0aad160ea2116
misskluck <sigrid.b.kluck@gmail.com>
2024-06-14 09:06:43 +0000

    added functions and logger

--- applications/server/main.js
+++ applications/server/main.js
@@ -1,8 +1,46 @@
+
 import { createServer } from 'node:http'
+
+
+function getAllReports(request, response) {
+    response.end("Here are all the reports\n")
+}
+
+function registerNewMeasurement(request, response) {
+    response.end("Report registered\n") 
+}
+
+function logger(request) {
+    const structuredLog = {
+        type: "info",
+        path: request.url,
+        timeStamp: new Date().toISOString(),
+    }
+    
+    console.log(structuredLog)
+}
 
 // Create a new server
 const server = createServer((request, response) => {
-    response.end("Hello World~\n")
+// Middleware --> setter opp noe logikk som skal ta og skje før vi sender det videre til de spesifikke handlerene
+logger(request)
+
+    const path = request.url
+
+// Routing logic
+switch (path) {
+    case "/reports":
+        getAllReports(request, response)
+        break;
+
+    case "/measurements":
+        registerNewMeasurement(request, response)
+        break;
+    default:
+        response.end("Resource not found")
+        break
+}
+    
 })
 
 // Start the server
