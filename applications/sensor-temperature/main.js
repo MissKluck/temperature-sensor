c496ce5f7105d8e5e509d35bd354d0af4852cd87
misskluck <sigrid.b.kluck@gmail.com>
2024-06-26 08:38:16 +0000

    add randomization to sensor readout

--- applications/sensor-temperature/main.js
+++ applications/sensor-temperature/main.js
@@ -1,12 +1,16 @@
 const serverAddress = "http://localhost:3000/measurements"
 
+function getRandomNumber(min, max) {
+  const range = max - min
+  ((Math.random() * range) + min)
+}
 
 function logTemperature() {
    
    const report = {
     sensorId: 0,
     measurementDate: new Date().toISOString(),
-    temperature: 15
+    temperature: getRandomNumber(15, 25).toFixed(2),
   }
  console.log("logging new report")
  console.log(report)
